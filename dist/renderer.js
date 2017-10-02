ace50913003f7008967968abbf6216037a1b8b4e
Christopher Breeden <github@u.breeden.cc>
2017-10-01 20:03:13 -0500

    update

---
+++ dist/renderer.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chessboard_js_1 = require("./chessboard.js");
+var board = new chessboard_js_1.default('chessboard');
+board.initialPosition();
+board.clearBoard();
+board.initialPosition();
